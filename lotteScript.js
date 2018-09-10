75261719a2184e1ca7059261cd521ad6b17d42ea
A-Game <javascript@adamheldring.com>
2018-09-10 15:52:56 +0200

    Cleaned up script

--- lotteScript.js
+++ lotteScript.js
@@ -1,12 +1,11 @@
 // LOTTE TEAM PROJEKT SCRIPT
 window.onload = function() {
 
-  var hero = document.getElementById('hero-image');
-  var heroHeight = hero.height + 50; //70px compensates for body TopMargin
+  var hero = document.getElementById('hero-image-container');
+  var heroHeight = 812; //70px compensates for body TopMargin
   var scrollPosition;
 
   function transformHero(e) {
-    heroHeight = hero.height + 50;
     scrollPosition = document.scrollingElement.scrollTop;
     if (scrollPosition < heroHeight) {
       hero.style.opacity = 1 - (scrollPosition / heroHeight);

280dcfc31406518c1ed812f9bc13ea78d9c59c0e
A-Game <javascript@adamheldring.com>
2018-09-12 17:27:39 +0200

    Mobile hamburger menu popup

--- navbarFunction.js
+++ navbarFunction.js
@@ -1,8 +1,22 @@
-function navbarFunction() {
-  var x = document.getElementById("navbar");
-  if (x.className === "navbar") {
-      x.className += " responsive";
-  } else {
-      x.className = "navbar";
+window.onload = function () {
+  var hamburger = document.getElementById('hamburger')
+  var mobileNavbar = document.getElementById("navbar");
+  console.log(mobileNavbar);
+
+  function navbarFunction() {
+    console.log(mobileNavbar);
+    if (mobileNavbar.className === "sticky navbar") {
+      console.log("yes");
+      console.log(mobileNavbar.className);
+        mobileNavbar.className = "sticky navbar responsive"
+    //     x.className += " responsive";
+    // } else {
+    //     x.className = "navbar";
+    } else {
+      console.log("no");
+      console.log(mobileNavbar.className);
+      mobileNavbar.className = "sticky navbar"
+    }
   }
+  hamburger.addEventListener("click", navbarFunction);
 }
